aa10ac474bd7657d3e6618a9abcd2052f2af37f7
Dustin Locke <s3165079@students.fscj.edu>
2025-02-16 17:12:45 -0500

    added home page and styling

--- frontend/FinancialClarity/src/app/app.component.ts
+++ frontend/FinancialClarity/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
+// import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, MatButtonModule],
+  imports: [HomeComponent, RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
