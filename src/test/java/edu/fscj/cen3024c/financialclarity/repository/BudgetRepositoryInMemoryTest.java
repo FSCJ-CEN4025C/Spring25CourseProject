8067c9821a943018bde1a97f8df838eb8a63b7d9
Dustin Locke <s3165079@students.fscj.edu>
2025-02-21 14:28:18 -0500

    fixed errors with tests

--- src/test/java/edu/fscj/cen3024c/financialclarity/repository/BudgetRepositoryInMemoryTest.java
+++ src/test/java/edu/fscj/cen3024c/financialclarity/repository/BudgetRepositoryInMemoryTest.java
@@ -30,9 +30,8 @@ public class BudgetRepositoryInMemoryTest {
         User savedUser = createUser("Tom1", "tom1@gmail.com");
 
         Budget budget = new Budget();
-        budget.setId(999);
         budget.setUser(savedUser);
-        budget.setCategoryId(999);
+        budget.setCategoryId(1);
         budget.setAmount(999.99);
         budget.setPeriod(Period.MONTHLY);
         budget.setStartDate(new Date());
@@ -42,15 +41,20 @@ public class BudgetRepositoryInMemoryTest {
 
         Budget foundBudget = entityManager.find(Budget.class, savedBudget.getId());
         assertThat(foundBudget).isEqualTo(budget);
+        assertThat(foundBudget.getUser()).isEqualTo(savedUser);
+        assertThat(foundBudget.getAmount()).isEqualTo(999.99);
+        assertThat(foundBudget.getPeriod()).isEqualTo(Period.MONTHLY);
+        assertThat(foundBudget.getStartDate()).isEqualTo(budget.getStartDate());
+        assertThat(foundBudget.getEndDate()).isEqualTo(budget.getEndDate());
+
     }
 
     @Test
     public void findByBudgetId_ShouldReturnBudget_WhenBudgetExists() {
         User savedUser = createUser("Tom2", "tom2@gmail.com");
         Budget budget = new Budget();
-        budget.setId(8888);
         budget.setUser(savedUser);
-        budget.setCategoryId(888);
+        budget.setCategoryId(1);
         budget.setAmount(888.88);
         budget.setPeriod(Period.WEEKLY);
         budget.setStartDate(new Date());
@@ -58,7 +62,7 @@ public class BudgetRepositoryInMemoryTest {
 
         entityManager.persist(budget);
 
-        Optional<Budget> optionalFoundBudget = budgetRepository.findById(888);
+        Optional<Budget> optionalFoundBudget = budgetRepository.findById(1);
         Budget foundBudget = optionalFoundBudget.get();
 
         assertThat(foundBudget).isNotNull();
@@ -68,7 +72,7 @@ public class BudgetRepositoryInMemoryTest {
 
 
     @Test
-    public void findByBudgetName_ShouldReturnEmpty_WhenBudgetDoesNotExist() {
+    public void findByBudgetId_ShouldReturnEmpty_WhenBudgetDoesNotExist() {
         Integer nonExistentBudgetId = 1000;
         Optional<Budget> foundBudget = budgetRepository.findById(nonExistentBudgetId);
         assertThat(foundBudget).isEmpty();
